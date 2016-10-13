0e92059f09cab65bda3c213c302b2bcbf04cfd30
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-10-13 15:24:38 -0500

    Add ignore file, more experimental changes

--- app.elm
+++ app.elm
@@ -2,26 +2,22 @@ import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-import List
+import Date exposing (Date)
+import Array
 import String
 
 {-
 
-an accounts field, that represents each account,  of type Account, which is a
-list of transactions and a starting balance The # of transactions kept in the
-model is limited, for performance reasons. The source of truth for transaction
-data is the bank account, not the app The starting balance is essentially an
-accumulator on data that needs to be cleaned out at the tail of the list
+payday budget type, which is the principal model in this app. It init's from
+data from the bank or CU with the balance of an account after a payday. The
+budget items are Transactions that either have happened, or haven't
+happened yet.
 
 The transaction data type is an amount (positive or negative), datetime, the
 payee, reason, and a notes field, which just captures any other notes from the
-transaction.
-
-The other fields are Name and AccountType
-
-The User type is for the person that is logged into the app
-
-The ScrapeIO type is what gets returned from pulls from institutions
+transaction. Typically, these are going to be credits, since the record keeping
+only persists between paydays. But I want to include unexpected windfalls as
+valid inputs
 
 The Payee type represents you are giving money to. It's just a name, a
 category, and notes
@@ -33,50 +29,54 @@ main =
 
 -- MODEL
 
-type alias Model = List Transaction
-
-
-type alias Account =
-        { name : String
-        , kind : AccountKind
-        , transactions : List Transaction
-        , balance : Int
+type alias Model =
+        { finishDate : Date
+        , startingBalance : Int
+        , transactions : Array Transaction
         }
 
 type alias Transaction =
-        { amount : String
-        --, datetime : DateTime
-        , payee : String
+        { datetime : Date
+        , amount : Int
         , notes : String
         }
 
-type AccountKind = Checking | Savings | CreditCard
-type ScrapeIO = NotSent | Waiting | Success | Failure
-
 model : Model
 model =
-        []
+        Model Date.now 0 Array.empty
 
-blankAccount : { account : Account, user: String }
-blankAccount =
-        { account = Account "Checking" Checking [] 0
-        , user = "Sam"
-        }
-
+initBudget : Date -> Int -> Date -> Model
+initBudget endDate startBalance =
+    Model endDate startBalance Array.empty
 
 -- UPDATE
 
-type Msg msg = AddTransaction msg | EditTransaction msg | ResetTransaction
+type Msg msg
+        = AddTransaction msg
+        | EditTransaction msg
+        | ResetTransaction
 
 update : Msg Transaction -> Model -> Model
 update msg model =
         case msg of
                 AddTransaction transaction ->
-                        transaction :: model
+                        Array.push transaction model
                 EditTransaction transaction ->
-                        
+                        editTransaction transaction model
                 ResetTransaction ->
-                        []
+                        Array.empty
+
+editTransaction : Transaction -> Model -> Model
+editTransaction transaction model =
+        let
+            (subject, rest) = head model
+            equal = subject.datetime == transaction.datetime
+        in
+            case equal of
+                    True ->
+                            transaction :: rest
+                    False ->
+                            (::) subject <| editTransaction transaction rest
 
 -- VIEW
 
@@ -84,14 +84,14 @@ view : Model -> Html msg
 view model =
         div []
                 [ viewTransactions model
-                , viewAddTransaction model
+                , viewAddTransaction
                 ]
 
 viewTransactions : Model -> Html msg
 viewTransactions transactions =
         table []
                 [ thead []
-                        [ th [] [text "Payee"]
+                        [ th [] [text "Time"]
                         , th [] [text "Amount"]
                         , th [] [text "Notes"]
                         ]
@@ -101,13 +101,14 @@ viewTransactions transactions =
 viewTransactionRow : Transaction -> Html msg
 viewTransactionRow transaction =
         tr []
-                [ td [] [text transaction.payee]
+                [ td [] [text transaction.datetime]
                 , td [] [text transaction.amount]
                 , td [] [text transaction.notes]
                 ]
 
-viewAddTransaction : Model -> Html msg
-vewAddTransaction model =
+viewAddTransaction : Html msg
+vewAddTransaction =
         form []
-                [ input [ type' "text", placeholder "Payee", onInput  
+                [ input [ type' "text", placeholder "Payee", onInput
                 ]
+
